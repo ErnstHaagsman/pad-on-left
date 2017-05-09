af15983593d14c67219665ec50a73e471adff840
Ernst Haagsman <ernst.haagsman@jetbrains.com>
2017-05-09 17:54:22 +0200

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,15 @@
+Left-pad for Python
+===================
+
+Installation:
+
+    pip install pad-on-left
+
+Usage
+
+    from pad_on_left.left_pad import left_pad
+
+    >>>left_pad('input', 10, '-')
+    -----input
+
+This package uses doctest for testing, run :code:`doctest left_pad.py` to run the tests.
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pad-on-left',
+    version='1.0.0',
+    packages=['pad_on_left'],
+    url='https://github.com/ErnstHaagsman/pad-on-left',
+    license='Apache 2.0',
+    author='Ernst Haagsman',
+    author_email='ernst.haagsman@jetbrains.com',
+    description='A python implementation of left-pad'
+)
